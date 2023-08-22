56a815eeabba50b6bb0f13b175e88db33849c84d
giabao1511 <giabao712411@gmail.com>
2023-08-22 12:29:41 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,155 @@
-import Image from 'next/image'
+"use client";
+
+import { Button, Checkbox, Form, Input } from "antd";
 
 export default function Home() {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  type FieldType = {
+    ApacheConfig?: string;
+    ServerName?: string;
+    MappingPort?: string;
+    APP_NAME?: string;
+    APP_ENV?: string;
+    APP_DEBUG?: boolean;
+    APP_KEY?: string;
+    APP_URL?: string;
+    ADMIN_FRONT_URL?: string;
+    WEB_FRONT_URL?: string;
+    LOG_CHANNEL?: string;
+    DB_CONNECTION?: string;
+    DB_HOST?: string;
+    DB_PORT?: number;
+    DB_DATABASE?: string;
+    DB_USERNAME?: string;
+    DB_PASSWORD?: string;
+    DB_SEARCH_PATH?: string;
+    DB_SSL_MODE?: string;
+    REDIS_HOST?: string;
+    REDIS_PASSWORD?: string;
+    SCRIBE_AUTH_KEY?: string;
+    CACHE_DRIVER?: string;
+    MAIL_DRIVER?: string;
+    SENTINEL_MULTIPLE_SESSION_MAX?: string;
+    QUEUE_CONNECTION?: string;
+    FILESYSTEM_DISK?: string;
+    MINIO_CDN?: string;
+    MINIO_ENDPOINT?: string;
+    MINIO_KEY?: string;
+    MINIO_SECRET?: string;
+    MINIO_BUCKET?: string;
+    MINIO_BUCKET_PROXY?: string;
+    MINIO_REGION?: string;
+    ELASTIC_APM_ENABLED?: boolean;
+    REDIS_QUEUE?: string;
+  };
+
+  const data = `  ApacheConfig exim.mangoads.com.vn.conf
+  ServerName exim.mangoads.com.vn
+  MappingPort http://localhost:6789
+
+  APP_NAME Laravel
+  APP_ENV Local
+  APP_DEBUG TRUE
+  APP_KEY	
+  APP_URL
+  ADMIN_FRONT_URL
+  WEB_FRONT_URL	
+  LOG_CHANNEL stderr
+    
+  DB_CONNECTION pgsql
+  DB_HOST 206.189.81.38
+  DB_PORT 5433
+  DB_DATABASE	
+  DB_USERNAME	
+  DB_PASSWORD	
+  DB_SEARCH_PATH	
+  DB_SSL_MODE prefer
+
+  REDIS_HOST 127.0.0.1
+  REDIS_PASSWORD	
+  SCRIBE_AUTH_KEY	
+    
+  CACHE_DRIVER file
+  MAIL_DRIVER	log
+  SENTINEL_MULTIPLE_SESSION_MAX 99999
+  QUEUE_CONNECTION database
+    
+  FILESYSTEM_DISK minio
+  MINIO_CDN https://cdn.mangoads.com.vn:9001
+  MINIO_ENDPOINT http://209.97.162.202:9000
+  MINIO_KEY	
+  MINIO_SECRET	
+  MINIO_BUCKET	
+  MINIO_BUCKET_PROXY	
+  MINIO_REGION us-east-1
+  ELASTIC_APM_ENABLED FALSE
+  REDIS_QUEUE default`;
+
+  console.log(data.split("\n"));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      {data?.split("\n")?.map((item, index) => {
+        const element = item.split(" ");
+        console.log(element);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        if (element?.length === 1 || element?.length === 5) {
+          return <br key={index} />;
+        }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        if (element?.[3] === "TRUE" || element?.[3] === "FALSE") {
+          return (
+            <div key={index}>
+              <Form.Item
+                label={element[2]}
+                name={element[2]}
+                valuePropName="checked"
+              >
+                <Checkbox></Checkbox>
+              </Form.Item>
+            </div>
+          );
+        } else {
+          return (
+            <div key={index}>
+              <Form.Item
+                label={element[2]}
+                name={element[2]}
+                rules={[
+                  {
+                    required: true,
+                    message: `Please input your ${element[2]}!`,
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </div>
+          );
+        }
+      })}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
